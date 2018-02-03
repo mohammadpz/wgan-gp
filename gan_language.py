1756257e2db57320a23e86317dceaa1a6028e980
Mohammad Pezeshki <mohammadpz@gmail.com>
2018-02-03 00:39:34 -0500

    making it even better

--- gan_language.py
+++ gan_language.py
@@ -19,7 +19,8 @@ from svd_plot import svdplot
 from sklearn.preprocessing import OneHotEncoder
 
 seed = np.random.randint(10000)
-seed = 1234
+# seed = 1234
+print('SEED: ' + str(seed))
 torch.cuda.manual_seed_all(seed)
 
 torch.manual_seed(seed)
