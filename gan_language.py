5b596629ab11f7383be80e2a9faf63330e215d0c
Mohammad Pezeshki <mohammadpz@gmail.com>
2018-02-04 19:58:10 -0500

    making it even better

--- gan_language.py
+++ gan_language.py
@@ -125,13 +125,13 @@ class SNConv2d(conv._ConvNd):
 
 class SNConv1d(conv._ConvNd):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
-        kernel_size = kernel_size
-        stride = _pair(stride)
-        padding = _pair(padding)
-        dilation = _pair(dilation)
+        kernel_size = (kernel_size)
+        stride = (stride)
+        padding = (padding)
+        dilation = (dilation)
         super(SNConv1d, self).__init__(
             in_channels, out_channels, kernel_size, stride, padding, dilation,
-            False, _pair(0), groups, bias)
+            False, (0), groups, bias)
         self.u = None
 
     def forward(self, input):
